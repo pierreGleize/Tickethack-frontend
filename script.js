0e1855dacf9b57abae65f9d80b3f193576076cf7
Pierre Gleize <gleizepierre3gmail.com>
2024-11-05 20:52:32 +0100

    Pierre Le fetch  pour récupérer les données à afficher marchent, il manque à afficher les heures

--- script.js
+++ script.js
@@ -4,35 +4,45 @@ search.addEventListener("click", () => {
   const departure = document.getElementById("departure").value;
   const arrival = document.getElementById("arrival").value;
   const calendar = document.getElementById("date").value;
-  console.log(departure, arrival, calendar);
-  //   fetch("http://localhost:3000/trips")
-  //     .then((response) => response.json())
-  //     .then((data) => {
-  //       if (!data.result) {
-  document.getElementById("image").src = "./images/notfound.png";
-  document.getElementById("trip-text").textContent = "No trip found.";
-  //   }else{
-  //   const trips = data.allTrips;
-  //   document.querySelector(".layout-right").textContent = "";
-  //   for (let i = 0; i < trips.length; i++) {
-  //     document.querySelector(".layout-right").textContent += `
-  //         <div>
-  //         <p>${trips.departure} > ${trips.arrival}</p>
-  //         <p>17:11</p>
-  //         <p>136€</p>
-  //         <button id='add-book${i}'>Book</button>
-  //         `;
-  //   }
-
-  //   }
-  // });
+  const trip = {
+    departure: departure,
+    arrival: arrival,
+    date: calendar,
+  };
+  console.log(typeof calendar);
+  fetch("http://localhost:3000/trips", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(trip),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.tripsFilter.length <= 0) {
+        document.querySelector("#layout-right-img-text").style.display = "flex";
+        document.getElementById("trips").style.display = "none";
+        document.getElementById("image").src = "./images/notfound.png";
+        document.getElementById("trip-text").textContent = "No trip found.";
+      } else {
+        const trips = data.tripsFilter;
+        document.querySelector("#layout-right-img-text").style.display = "none";
+        document.getElementById("trips").style.display = "block";
+        for (let i = 0; i < trips.length; i++) {
+          const travelDiv = document.createElement("div");
+          travelDiv.classList.add("travel");
+          travelDiv.innerHTML = `
+           <p>${trips[i].departure} > ${trips[i].arrival}</p>
+            <p>17:11</p>
+           <p>${trips[i].price}€</p>
+           <button id='add-book-${i}' class='add-book'>Book</button>
+           `;
+          document.getElementById("trips").appendChild(travelDiv);
+        }
+        const addBook = document.querySelectorAll(".add-book");
+        addBook.forEach((element) => {
+          element.addEventListener("click", (event) => {
+            console.log(event.target.parentNode);
+          });
+        });
+      }
+    });
 });
-
-document.querySelector(".layout-right").innerHTML = `
-<div class='travel'>
-<p>Paris > Lyon</p>
-<p>17:11</p>
-<p>136€</p>
-<button id='add-book'>Book</button>
-</div>
-`;
